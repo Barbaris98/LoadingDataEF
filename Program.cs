d20001aff989c2ba95803e27531cd06e7e12b73a
Dmitry Grigorev <d-d12@mail.ru>
2023-01-22 21:39:45 +0400

    Lazy loading

--- Program.cs
+++ Program.cs
@@ -13,40 +13,33 @@ namespace LoadingData
                 db.Database.EnsureDeleted();
                 db.Database.EnsureCreated();
 
-                Country usa = new Country { Name = "USA" };
-                Country japan = new Country { Name = "Japan" };
-                db.Countries.AddRange(usa, japan);
-
                 // добавляем начальные данные
-                Company microsoft = new Company { Name = "Microsoft", Country = usa };
-                Company sony = new Company { Name = "Sony", Country = japan };
-                db.Companies.AddRange(microsoft, sony);
+                Company microsoft = new Company { Name = "Microsoft" };
+                Company google = new Company { Name = "Google" };
+                db.Companies.AddRange(microsoft, google);
 
 
                 User tom = new User { Name = "Tom", Company = microsoft };
-                User bob = new User { Name = "Bob", Company = sony };
+                User bob = new User { Name = "Bob", Company = google };
                 User alice = new User { Name = "Alice", Company = microsoft };
-                User kate = new User { Name = "Kate", Company = sony };
+                User kate = new User { Name = "Kate", Company = google };
                 db.Users.AddRange(tom, bob, alice, kate);
 
                 db.SaveChanges();
             }
-            // получение данных
             using (ApplicationContext db = new ApplicationContext())
             {
-                // получаем пользователей
-                var users = db.Users
-                    .Include(u => u.Company)  // подгружаем данные по компаниям
-                        .ThenInclude(c => c!.Country)    // к компаниям подгружаем данные по странам
-                    .ToList();         // (null-forgiving оператор), чтобы указать, что значение null в данной ситуации невоможно
-                foreach (var user in users)
-                    Console.WriteLine($"{user.Name} - {user.Company?.Name} - {user.Company?.Country?.Name}");
+                var users = db.Users.ToList();
+                foreach (User user in users)
+                    Console.WriteLine($"{user.Name} - {user.Company?.Name}");
             }
+            /*
+            Однако при использовании lazy loading следует учитывать что если в базе данных
+            произошли какие-нибудь изменения, например, другой пользователь изменил данные,
+            то данные не перезагужаются, контекст продолжает использовать те данные, которые
+            были ранее загружены, как собственно было показано выше.
 
-
-
-
-
+            */
         }
     }
 }
